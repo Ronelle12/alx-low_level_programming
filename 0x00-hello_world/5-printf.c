ad7b2ca0e0d1ee259f3a23ae7f85aedfd61d5029
Ronelle <ronellekgaphola@gmail.com>
2022-06-02 15:08:28 -0700

    .

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - print string to screen
+ *
+ * Retun: 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return(0);
+	return (0);
 }
