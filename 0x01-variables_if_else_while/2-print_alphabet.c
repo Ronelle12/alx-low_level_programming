08641af4810c2f24da77a1139731eb8c3c282900
ronelle12 <ronellekgaphola@gmail.com>
2022-06-03 21:40:18 +0000

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the alhabet in small letters
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+/* print from a - z */
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+
+	return (0);
+}
