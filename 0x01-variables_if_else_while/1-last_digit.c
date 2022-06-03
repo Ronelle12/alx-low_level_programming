fc86204a4c23da2be8ce6f4f966da171084bebb1
ronelle12 <ronellekgaphola@gmail.com>
2022-06-03 20:37:26 +0000

    .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 5)
+	if (n > 5 && n != 0)
 	{
 /* prints a value greater than 5 to the console */
 		a = n % 10;
