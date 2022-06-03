e61f7d0abcf113b334a8a1e0327c4547fd6bb180
ronelle12 <ronellekgaphola@gmail.com>
2022-06-03 22:53:58 +0000

    .

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Prints letters of the Alphabet with exceptions
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (n = 'a'; n <
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - looks at single decimal dgits and prints
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	for (num = 0; num <= 10; num++)
+	{
+/* print input */
+		putchar(num);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
