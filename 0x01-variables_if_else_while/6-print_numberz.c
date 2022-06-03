57a5ffa2c045ede6c52537f545b348cb9a87355e
ronelle12 <ronellekgaphola@gmail.com>
2022-06-03 23:07:06 +0000

    .

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - looks at single decimal dgits and prints
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	/* print input */
+		putchar((num % 10) + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
