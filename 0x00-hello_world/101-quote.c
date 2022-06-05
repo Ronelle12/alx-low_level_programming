331d20a93b4275f5cf7f2035757ffb96e9ab033a
ronelle12 <ronellekgaphola@gmail.com>
2022-06-05 19:42:37 +0000

    .

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib>
+/**
+ * main - print literal screen to console
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char buffer[256] = "2015-10-19";
+
+	printf("\"and that piec of art is useful\" - Dora Korpar, %c\n", buffer);
+
+	return (0);
+
+}
