aad4e566ccdd29229d5585bfae9d00b0e190722e
Ronelle <ronellekgaphola@gmail.com>
2022-06-07 13:39:52 -0700

    .

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 : print 10 times the alphabet in lowercase
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
  *
  * Return: 0
  */
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int i;
 	char c;
 
-	for (i = 0; i <= 10; ++i)
+	for (i = 0; i <= 9; ++i)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
