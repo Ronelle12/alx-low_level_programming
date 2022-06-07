5c6f89a9679e976621eaf23f9995fb6d121e32ab
Ronelle <ronellekgaphola@gmail.com>
2022-06-07 12:29:45 -0700

    .

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - print string to screen
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(c[i]);
+
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this folder contains tasks for nested loops
