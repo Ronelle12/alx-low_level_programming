48d99952aeb7243c2cc9734a01cf5811896bbb3d
Ronelle <ronellekgaphola@gmail.com>
2022-06-09 13:21:34 -0700

    .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0:
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		++i;
+	}
+	_putchar('\n');
+}
