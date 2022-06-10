5fc9e05a44dfbaecac858c5f37e700c9e2bf0cc6
ronelle12 <ronellekgaphola@gmail.com>
2022-06-10 14:51:02 +0000

    .

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triaangular shape using #'s
+ * @size: the height of the triagle.
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size i++)
+		{
+			for (j = size; j >= 1; j--)
+			{
+			if (i < j)
+				_putchar(' ');
+			else
+				_putchar('#');
+			}
+			_putchar('\n');
+
+		}
+	}
+		else
+			_putchar('\n');
+}
