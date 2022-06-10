813d359e6fbb96fb65fa145c83b23585e3820322
ronelle12 <ronellekgaphola@gmail.com>
2022-06-10 18:41:36 +0000

    .

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include<stdio.h>
+#include "main.h"
 /**
  * main - checks a random input number and prints to scree
  * if it is positve, negative or zero
@@ -9,26 +7,10 @@
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-	/* prints when input is positive */
-	printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-	/* prints when input is negative */
-	printf("%d is negative\n", n);
-	}
-	else if (n == 0)
-	{
-	/* prinst when input is zero */
-	printf("%d is zero\n", n);
-	}
+	i = 98;
+	positive_or_negative(i);
 
 	return (0);
 }
