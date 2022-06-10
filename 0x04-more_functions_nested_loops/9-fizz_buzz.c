e203d2bd9b4094d1238a08869bad3654942d8574
ronelle12 <ronellekgaphola@gmail.com>
2022-06-10 14:29:18 +0000

    .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("Fizzbuzz");
 		else if (i % 3 == 0)
-			printf("FIzz");
+			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else
