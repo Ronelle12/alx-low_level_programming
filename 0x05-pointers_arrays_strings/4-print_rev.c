fd8c7a8c2785b5274ee17c8205460a5603444602
ronelle12 <ronellekgaphola@gmail.com>
2022-06-13 19:41:51 +0000

    .

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (i > '0')
+	while (i >= '0')
 	{
 		if (s[i] == '\0')
 			break;
@@ -17,8 +17,7 @@ void print_rev(char *s)
 
 	}
 	for (i--; i >= 0; i--)
-	{
 		_putchar(s[i]);
-	}
+	
 	_putchar('\n');
 }
