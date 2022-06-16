940d52615872d3b988887a1988396d2f6419687c
ronelle12 <ronellekgaphola@gmail.com>
2022-06-16 15:21:44 +0000

    .

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,3 +13,5 @@ void print_number(int n);
 char *infinite_add(char *ni, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);p
 int _putchar(char c);
+
+#endif
