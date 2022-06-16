5fcb253958d5517aa5edb8d69e245c8393495ca6
ronelle12 <ronellekgaphola@gmail.com>
2022-06-16 15:05:25 +0000

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,8 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0; j = 0;
+	int i = 0;
+	int j = 0;
 
 	while (*(dest + i) != '\0')
 	{
@@ -17,7 +18,7 @@ char *_strcat(char *dest, char *src)
 
 	while (j >= 0)
 	{
-		*(dest = i) = *(src + j);
+		*(dest + i) = *(src + j);
 		if (*(src + j) == '\0')
 			break;
 		i++;
