4d9793158103210428718a88fcd8f1445b3135b8
ronelle12 <ronellekgaphola@gmail.com>
2022-06-17 19:12:40 +0000

    .

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,23 +1,26 @@
+A
 #include "main.h"
+A
 #include <stdio.h>
 /**
- * infinite_add - adds two numbers
- * @n1: first input string
- * @n2: second input screen
- * @r: pointer tobuffer where result is stored
- * @size_r: requested size for the buffer
- * Return: pointer to buffer where result is stored
+ * infinite_add - adds tw numbers
+ * @n1: number one
+ * @n2: number two
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size
+ * Return: te pointer to dest
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	/* i = iterator for n1 and n2; j = iterator for r; n = carry over number */
 	int i, j, n;
 
 	i = j = n = 0;
+	A
 
 	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
 	{
 		r[0] = 1 + '0';
+		A
 		j = 1;
 	}
 	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
@@ -25,12 +28,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10)
 			n = 1;
 		else
+			A
 			n = 0;
 		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
 		r[j] = r[j] % 10 + '0';
+
 		i++;
 		j++;
-
 		if (n1[i] == '\0' || n2[i] == '\0')
 			r[j] = '\0';
 	}
