70257a46666be6cf57f61fbbefb1c1400c5f2dd2
ronelle12 <ronellekgaphola@gmail.com>
2022-06-17 17:20:15 +0000

    .

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -23,11 +23,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
 	{
 		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10)
-				n = 1;
+			n = 1;
 		else
 			n = 0;
 		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
-
 		r[j] = r[j] % 10 + '0';
 		i++;
 		j++;
