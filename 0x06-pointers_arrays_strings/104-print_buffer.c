35a3f9d97a8d240fe33d5c5611bddf2fdc33310f
ronelle12 <ronellekgaphola@gmail.com>
2022-06-17 20:12:01 +0000

    .

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,7 +11,7 @@ void print_buffer(char *b, int size)
 	int j, k, l;
 
 	if (size <= 0)
-	
+
 		printf("\n");
 	else
 	{
@@ -32,10 +32,10 @@ void print_buffer(char *b, int size)
 			{
 				if (l >= size)
 					break;
-				if (*(b + 1) < 32 || *(b + 1) > 126)
+				if (*(b + l) < 32 || *(b + l) > 126)
 					printf("%c", '.');
 				else
-					printf("%c", *(b + 1));
+					printf("%c", *(b + l));
 			}
 			printf("\n");
 		}
